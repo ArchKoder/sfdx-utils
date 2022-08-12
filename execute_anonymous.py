09de6888f6fad5ff599e9af4d8f797ea3280773f
ArchKoder <archkoder@protonmail.com>
2022-08-13 01:08:09 +0530

    updateExecuteAnonymous

--- execute_anonymous.py
+++ execute_anonymous.py
@@ -1,30 +1,18 @@
 from subprocess import run
-from os import getcwd
-from os.path import getmtime as lastModifiedTime
-from os import listdir as listDir
-from os.path import isfile as isFile
 from datetime import datetime
-
-def getLastModifiedFileName(targetDir):
-    def isApexFile(f):
-        if isFile(targetDir+f):
-            if f.endswith('.apex'):
-                return True
-        return False
-
-    files = [(f, lastModifiedTime(targetDir+f)) for f in listDir(targetDir) if isApexFile(f)]
-    files = sorted(files, key=lambda x: x[1], reverse=True)
-    return files[0][0]
+from utilities import getApexScriptDir
+from utilities import getLastModifiedFileName
+from sfdxUtilitesConstants import FILE_TYPE_APEX
+from sfdxCommandFunctions import forceApexExecute
+from utilities import getDefaultOrg
 
 if __name__ == "__main__":
-    wd,scriptDir= getcwd() , '/scripts/apex/'
-    projectName = wd[wd.rfind('/')+1:]
-    targetDir = wd+scriptDir
-    lastModifiedScript = getLastModifiedFileName(targetDir)
-    cmnd = 'sfdx force:apex:execute '
-    cmnd += '-u '+projectName+'.com '
-    cmnd += '-f '+targetDir+lastModifiedScript
-    cmnd += ' --json'
+    username = input("enter org username/alias : ")
+    if (username==''):
+        username = getDefaultOrg()
+    targetDir = getApexScriptDir()
+    lastModifiedScript = getLastModifiedFileName(targetDir,FILE_TYPE_APEX)
+    cmnd = forceApexExecute(targetDir,lastModifiedScript)
     print(cmnd)
     run(cmnd,shell=True)
     print('ran '+lastModifiedScript+' at '+datetime.now().strftime("%H:%M:%S"))
