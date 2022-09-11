566f2956ae1ff508eeec6795af8c3197c9306ada
ArchKoder <archkoder@protonmail.com>
2022-09-11 14:52:41 +0530

    merge manifests now considers final input to be filename if it is not a number else uses number to map to existing file's filename

--- mergeManifests.py
+++ mergeManifests.py
@@ -9,7 +9,16 @@ if __name__ == "__main__":
     searchTerm = input('Enter search term for the manifests to merge:')
     if searchTerm =='':
         searchTerm=None
-    filenameList = fileSelector(getManifestDir(),FILE_TYPE_XML,searchTerm)
-    finalManifestName = input("Enter desired name of the merged manifest:\n")
+    indexToFileName,fileIndexList = fileSelector(getManifestDir(),FILE_TYPE_XML,searchTerm,True,'Enter space seperated indexes for manifests to merge')
+    filenameList=[]
+    for index in indexToFileName.keys():
+        if (index in fileIndexList):
+            filenameList.append(indexToFileName[index])
+    indexToFileName,fileInputList = fileSelector(getManifestDir(),FILE_TYPE_XML,'',False,'Enter the file Index or new name for target manfest')
+
+    if fileInputList[0] in indexToFileName.keys():
+        finalManifestName = indexToFileName[fileInputList[0]]
+    else:
+        finalManifestName = fileInputList[0]
+
     mergeManifests(filenameList,getManifestDir(),finalManifestName)
-    
