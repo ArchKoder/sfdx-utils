49bcb44e1168c66d343c1eb287b64d8de3743576
ArchKoder <archkoder@protonmail.com>
2022-09-28 01:44:58 +0530

    add different error and file constants

--- sfdxUtilitesConstants.py
+++ sfdxUtilitesConstants.py
@@ -1,5 +1,8 @@
-FILE_TYPE_XML='xml'
-FILE_TYPE_APEX='apex'
+FILE_TYPE_XML = 'xml'
+FILE_TYPE_APEX = 'apex'
+FILE_TYPE_CSV = 'csv'
+FILE_TYPE_XLSX = 'xlsx'
+FILE_TYPE_JSON = 'json'
 
 MANIFEST_XML='<?xml version="1.0" encoding="UTF-8"?>'
 MANIFEST_PACKAGE='<Package xmlns="http://soap.sforce.com/2006/04/metadata">'
@@ -24,3 +27,8 @@ APEX_CODE_FILE = '--apexcodefile'
 SFDX_CONFIG_JSON_DEFAULTUSERNAME='defaultusername'
 
 ORG_USERNAME_ALIAS = 'Enter username or alias for org: '
+
+INVALID_PATH = 'Invalid path for file or file not found'
+INVALID_FILE_FORMAT = 'Invalid or unsupported format for the file'
+
+OBJECT_API_NAME = 'objectApiName'
