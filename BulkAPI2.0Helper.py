9446c22c2a60dd0fd6f37e7401f3f03fd40da48a
ArchKoder <archkoder@protonmail.com>
2022-10-24 02:48:34 +0530

    set up BulkAPIHelper

---
+++ BulkAPI2.0Helper.py
@@ -0,0 +1,36 @@
+from platformHelper import PlatformHelper
+from sfdxUtilitesConstants import (ASSIGNMENT_RULE_ID, COLUMN_DELIMITER,
+                                   CONTENT_TYPE, EXTERNAL_ID_FIELD_NAME,
+                                   LINE_ENDING, OBJECT, OBJECT_API_NAME,
+                                   OPERATION, OPERATION_INSERT)
+
+
+class BulkAPIHelper:
+
+    def __init__(this,**kwargs):
+        this.platformHelper = PlatformHelper()
+
+    def createJob(this,**kwargs):
+
+        createRequestBody = {
+            LINE_ENDING : this.platformHelper.lineEnding
+        }
+
+        for key,value in kwargs.items():
+            if this.validateRequestProperty(key,value):
+                createRequestBody.put(key,value)
+
+        
+
+    def validateRequestProperty(this,property,value):
+        if property in [OPERATION,OBJECT]:
+            if value == None:
+                raise Exception(property+' is mandatory!')
+            else:
+                return True
+        elif property in [ASSIGNMENT_RULE_ID,COLUMN_DELIMITER,CONTENT_TYPE,EXTERNAL_ID_FIELD_NAME]:
+            if value != None:
+                return True
+
+        else:
+            return False
